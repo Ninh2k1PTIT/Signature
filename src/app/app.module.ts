b76940d5f1f7e5e378f9aabcdb2ed5e84bd8309d
unknown <nguyentienhaininh@gmail.com>
2022-08-15 22:27:25 +0700

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CheckBoxModule, ButtonModule } from '@syncfusion/ej2-angular-buttons';
+import { DropDownListModule } from '@syncfusion/ej2-angular-dropdowns';
+import { ColorPickerModule, UploaderModule, SignatureModule, TextBoxModule, NumericTextBoxModule } from '@syncfusion/ej2-angular-inputs';
+import { TabModule, ToolbarModule } from '@syncfusion/ej2-angular-navigations';
+import { DialogModule } from '@syncfusion/ej2-angular-popups';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    ColorPickerModule,
+    BrowserModule,
+    CheckBoxModule,
+    DropDownListModule,
+    TabModule,
+    ToolbarModule,
+    UploaderModule,
+    SignatureModule,
+    TextBoxModule,
+    DialogModule,
+    ButtonModule,
+    NumericTextBoxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
